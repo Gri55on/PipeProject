7c32fbdb0547971ddf6d3e1a27f487c6cfe9c3fa
Gareth Griffiths <exauroura@yahoo.com>
2015-07-29 15:43:43 +0100

    Basic implementation done
    Pipe movement, on entry/exit logic

--- Assets/Scripts/Pipe.cs
+++ Assets/Scripts/Pipe.cs
@@ -3,20 +3,44 @@ using System.Collections;
 
 public class Pipe : MonoBehaviour 
 {
+	 public float flowSpeed = 10.0f;
+
     //Pipe logic
-	 void OnTriggerStay(Collider other)
-    {
-        //Turns off gravity
-        other.gameObject.GetComponent<Rigidbody>().useGravity = false;
-        
-        //Test freeze of object
-        other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    //On entering pipe
+    void OnTriggerEnter(Collider other)
+    { 
+		//If the object that hits us is already colliding with a object that constains this scipt, do nothing.
 
-        //Add Plain force
-        other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 1, 0));
+		Debug.Log("Entered");
+		//Turns off gravity
+		other.gameObject.GetComponent<Rigidbody>().useGravity = false;
+		
+		//Test freeze of object
+		other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
 
-        //Add Relative force
-         other.gameObject.GetComponent<Rigidbody>().AddRelativeForce(new Vector3 (0,1,0));
     }
+
+    //While in pipe
+    void OnTriggerStay(Collider other)
+    {
+		Debug.Log("Current");
+
+		//Local pipe transform
+		// How to store and apply transforms
+		Vector3 pipeV = this.transform.TransformVector(new Vector3(0, flowSpeed, 0));
+
+		//Adds a force relative to the direction of the pipe transform
+		other.gameObject.GetComponent<Rigidbody>().AddForce(pipeV);
+		
+    }
+
+	//On Pipe Exit
+	void OnTriggerExit(Collider other)
+	{
+		Debug.Log("Exit");
+		//Object affected by gravity once it has left the pipe
+		other.gameObject.GetComponent<Rigidbody>().useGravity = true;
+
+	}
 	
 }
